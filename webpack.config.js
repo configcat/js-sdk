88330259a7353edbeee646a230545e3c7e1f7c34
sige <sige@configcat.com>
2018-11-09 17:45:48 +0100

    pure sample app

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/indexPure.ts",
+  output: {
+    filename: "configcat.js"
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
