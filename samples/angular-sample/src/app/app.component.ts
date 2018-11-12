bd94ab175d5ed08201a0ee8bfa06864bc1419ed4
sige <sige@configcat.com>
2018-11-12 18:21:48 +0100

    angular sample progress

--- samples/angular-sample/src/app/app.component.ts
+++ samples/angular-sample/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as configcat from 'configcat-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sample';
+
+  public result: String;
+
+  click() {
+  }
 }
