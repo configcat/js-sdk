16388ce479b467120cf9d1321b11d38f7ec1a3b2
ConfigCat <developer@configcat.com>
2018-11-13 11:38:39 +0100

    LocalStorage cache too

--- src/Cache.ts
+++ src/Cache.ts
@@ -6,9 +6,36 @@ export class LocalStorageCache implements ICache {
 
     Set(apiKey: string, config: ProjectConfig): void {
         this.cache[apiKey] = config;
+
+        try {
+            localStorage.setItem(this.getLocalStorageKey(apiKey), btoa(JSON.stringify(config)));
+        } catch (ex) {
+            // local storage is unavailable
+        }
     }
 
     Get(apiKey: string): ProjectConfig {
-        return this.cache[apiKey];
+        let config = this.cache[apiKey];
+        if (config) {
+            return config;
+        }
+
+        try {
+            let configString = localStorage.getItem(this.getLocalStorageKey(apiKey));
+            if (configString) {
+                let config = JSON.parse(atob(configString));
+                if (config) {
+                    return config;
+                }
+            }
+        } catch (ex) {
+            // local storage is unavailable or invalid cache value in localstorage
+        }
+
+        return null;
+    }
+
+    private getLocalStorageKey(apiKey: string): string{
+        return "ConfigCat_" + apiKey;
     }
 }
