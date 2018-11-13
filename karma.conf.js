180f6a258aab462265846e8b292d6c15f76bf616
ConfigCat <developer@configcat.com>
2018-11-13 12:57:46 +0100

    Coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -12,10 +12,20 @@ module.exports = function(config) {
             "**/*.ts": ["karma-typescript"]
         },
 
-        reporters: ["progress", 'karma-typescript'],
+        coverageReporter: {
+            // specify a common output directory
+            dir: 'coverage',
+            reporters: [
+              { type: 'lcov', subdir: 'report-lcov' },
+              { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+            ]
+          },
+
+        reporters: ["progress", "coverage"],
 
         browsers: ["ChromeHeadless"],
 
         singleRun: true
+
     });
 };
