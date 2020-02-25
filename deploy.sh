e884c23858e48c07a846a23d4c0bf51d01caa6bf
sige <sige@configcat.com>
2020-02-25 14:42:52 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #Run this script to update configcat-common to the latest and release a new version of configcat-js
 set -e #Making sure script stops on error
 npm i configcat-common@latest
+npm test
 git add .
 git commit -m "updating configcat-common"
 git push origin $(npm version patch)
