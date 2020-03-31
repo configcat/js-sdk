47929b95240bd738c4583cfe175efebf0a0e14b4
sige <sige@configcat.com>
2020-03-31 22:58:25 +0200

    react native sample progress

--- samples/react-native/App.js
+++ samples/react-native/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as ConfigCat from 'configcat-js';
 
 export default function App() {
+
+  const configcatClient = ConfigCat.createClientWithAutoPoll("7ZTVCKnUJprikI6Rwlj0RA/eJ8H21HZA06fDJrnzWyvGA", { pollIntervalSeconds: 2 });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Is my awesome feature enabled?</Text>
+        <Demo client={configcatClient}></Demo>
+    </View>
+  );
+}
+
+function Demo(props) {
+  const [awesomeValue, setAwesomeValue] = useState(false);
+  props.client.getValueAsync("isAwesomeFeatureEnabled", false).then(value => {
+    setAwesomeValue(value);
+  });
+  return (
+    <View style={styles.demo}>
+      <Text style={styles.value}>{awesomeValue ? "yes" : "no"}</Text>
+      <Button
+        onPress={() => {
+          props.client.getValueAsync("isAwesomeFeatureEnabled", false).then(value => {
+            setAwesomeValue(value);
+          });
+        }}
+        title={"Check feature flag"}
+      />
     </View>
   );
 }
@@ -16,4 +41,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  value: {
+    margin:20
+  },
+
+  demo: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
 });
