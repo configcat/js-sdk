6c01a71b14cf9e84f1158164a2bfe042a0808a4c
sige <sige@configcat.com>
2022-03-30 16:40:01 +0200

    new angular

--- samples/ab-testing-with-amplitude-sample/src/app/app.component.ts
+++ samples/ab-testing-with-amplitude-sample/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as configcat from 'configcat-js';
+import { IConfigCatClient } from 'configcat-common/lib/ConfigCatClient';
+import { LogLevel, User } from 'configcat-common';
+import amplitude from 'amplitude-js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ab-testing-with-amplitude-sample';
+  title = 'amplitude-abtest-sample';
+
+  constructor() {
+    // Setting log level to Info to show detailed feature flag evaluation in the console.
+    const logger = configcat.createConsoleLogger(LogLevel.Info);
+
+    // Initializing the ConfigCat client, it should be initialized as a singleton.
+    // Get your ConfigCat SDK key here: https://app.configcat.com/sdkkey
+    this.configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/wAuE2NZ270KuN9utBYlBjg',
+      {
+        pollIntervalSeconds: 2,
+        logger: logger
+      });
+
+  }
+
+  ngOnInit() {
+    // Initialize Amplitude with your API_KEY to upload tracking data. API_KEY: https://help.amplitude.com/hc/en-us/articles/360058073772#view-and-edit-your-project-information
+    amplitude.getInstance().init('YOUR_AMPLITUDE_API_KEY');
+
+    // The following steps are necessary for creating a funnel diagram in Amplitude to visualize how many times A or B version of the card was loaded and clicked.
+
+    // ConfigCat userObject (https://configcat.com/docs/advanced/user-object) with a unique identifier is necessary to download and evaluate feature flag values.
+    // There are no logged in users in the sample application to take a user ID from. 
+    // Instead I'm taking the DeviceID from Amplitude as a unique identifier: https://developers.amplitude.com/docs/identify-api
+    const userObject = new User(amplitude.getInstance().options.deviceId);
+    console.log(amplitude.getInstance().options.deviceId);
+
+    // Getting all the evaluated feature flag keys and values from ConfigCat for the given userObject.
+    this.configCatClient.getAllValuesAsync(userObject).then((values) => {
+
+      // Getting the identity from Amplitude: https://developers.amplitude.com/docs/identify-api
+      const identity = new amplitude.Identify();
+
+      // Setting the evaluated feature flag keys and values on the identity object.
+      values.forEach(i => { identity.set(i.settingKey, i.settingValue); });
+
+      // Uploading the identity object together with the evaluated feature flag values to Amplitude.
+      amplitude.getInstance().identify(identity);
+
+      // Sending the 'visited' event to Amplitude to track the page visit as the first step in the funnel. 
+      amplitude.getInstance().logEvent('visited', { 'title': this.title });
+    });
+  }
+
+  public configCatClient: IConfigCatClient;
 }
--- samples/ab-testing-with-amplitude-sample/src/app/app.module.ts
+++ samples/ab-testing-with-amplitude-sample/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
