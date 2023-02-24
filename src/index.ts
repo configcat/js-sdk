2c5881b17ea4ed9742f2bc9e13b34430d1c580c8
Adam Simon <adamosimoni@gmail.com>
2023-02-24 18:26:35 +0100

    Fix JSDoc of getClient

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import CONFIGCAT_SDK_VERSION from "./Version";
  * Returns an instance of ConfigCatClient for the specified SDK Key.
  * @remarks This method returns a single, shared instance per each distinct SDK Key.
  * That is, a new client object is created only when there is none available for the specified SDK Key.
- * Otherwise, the already created instance is returned (in which case the 'pollingMode', 'options' and 'configCatKernel' arguments are ignored).
+ * Otherwise, the already created instance is returned (in which case the 'pollingMode' and 'options' arguments are ignored).
  * So, please keep in mind that when you make multiple calls to this method using the same SDK Key, you may end up with multiple references to the same client object.
  * @param sdkKey SDK Key to access configuration
  * @param pollingMode The polling mode to use
