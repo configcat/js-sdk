4a16a3940e8a74ac5efcd519ded6fc33bad4d66e
adams85 <31276480+adams85@users.noreply.github.com>
2023-07-26 17:04:49 +0200

    Prepare v8.1.0 release (#82)
    * Update to configcat-common v8.1.0
    
    * Fix dependency vulnerabilities
    
    * Bump version
    
    * Add public API re-exports

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ export type { IConfig, ISetting, ITargetingRule, IPercentageOption, SettingValue
 
 export { SettingType, Comparator } from "configcat-common";
 
-export type { IConfigCatClient } from "configcat-common";
+export type { IConfigCatClient, IConfigCatClientSnapshot } from "configcat-common";
 
 export { SettingKeyValue } from "configcat-common";
 
@@ -112,6 +112,8 @@ export { OverrideBehaviour } from "configcat-common";
 export { RefreshResult } from "configcat-common";
 
 export type { IProvidesHooks, HookEvents } from "configcat-common";
+
+export { ClientReadyState } from "configcat-common";
 
 /* Default export */
 
