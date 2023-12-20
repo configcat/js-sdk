a472443872a7286d59e84908c786dcb3d97de845
Lajos Szoke <lali@configcat.com>
2023-12-20 15:59:49 +0100

    codecleanup

--- src/Cache.ts
+++ src/Cache.ts
@@ -25,13 +25,13 @@ export class LocalStorageCache implements IConfigCatCache {
   }
 
   private b64EncodeUnicode(str: string): string {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (_, p1) {
       return String.fromCharCode(parseInt(p1, 16))
     }))
   }
 
   private b64DecodeUnicode(str: string): string {
-    return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function (c: string) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
     }).join(''))
   }
