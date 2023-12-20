fa55900034463d487388e0f3ac587af192138b88
Lajos Szoke <lali@configcat.com>
2023-12-20 16:00:51 +0100

    cache

--- src/Cache.ts
+++ src/Cache.ts
@@ -20,19 +20,18 @@ export class LocalStorageCache implements IConfigCatCache {
     catch (ex) {
       // local storage is unavailable or invalid cache value in localstorage
     }
-
     return void 0;
   }
 
   private b64EncodeUnicode(str: string): string {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (_, p1) {
       return String.fromCharCode(parseInt(p1, 16))
-    }))
+    }));
   }
 
   private b64DecodeUnicode(str: string): string {
     return decodeURIComponent(Array.prototype.map.call(atob(str), function (c: string) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(''))
+    }).join(''));
   }
 }
